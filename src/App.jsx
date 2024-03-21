7631ffa62ed63c8012898f0e133532b116a8d715
Arslan Javaid <aja118379@gmail.com>
2024-03-22 00:26:51 +0500

    Created Post and Posts components

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import './App.css'
 import Footer from './components/Footer'
 import Header from './components/header'
 import Sidebar from './components/Sidebar'
+import CreatePost from './components/CreatePost'
+import Posts from './components/Posts'
 
 function App() {
 
@@ -10,7 +12,10 @@ function App() {
       <Sidebar />
       <div className='content'>
         <Header />
+          <CreatePost />
+          <Posts />
         <Footer />
+
       </div> 
     </div>
   )
---
+++ src/components/Posts.jsx
@@ -0,0 +1,13 @@
+import Post from '../components/Post'
+
+const Posts = () => {
+  return (
+    <>
+        <Post />
+        <Post />
+        <Post />
+    </>
+  )
+}
+
+export default Posts
